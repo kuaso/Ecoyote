8e7be751539f533a022c73597a834e197242357d
Cloudate9 <65006925+Cloudate9@users.noreply.github.com>
2023-06-23 23:18:10 -0700

    Replace placeholder sprite with coyote

--- Assets/MovementScript.cs
+++ Assets/MovementScript.cs
@@ -22,24 +22,24 @@ public class MovementScript : MonoBehaviour
 
         if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.Space))
         {
-            coyoteRigidbody.velocity = Vector2.up * 20;
+            coyoteRigidbody.velocity = Vector2.up * 6.5f;
         }
         
         if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
         {
-            x -= 10;
+            x -= 2;
         }
         
         if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
         {
-            x += 10;
+            x += 2;
         }
         
         if (x == 0)
         {
             // Take the current velocity and take 90% of its x value to slow down
-            var alteredXVelocity = (coyoteRigidbody.velocity.x * 0.85f);
-            if (Math.Round(alteredXVelocity, 5) != 0) x = alteredXVelocity; 
+            var alteredXVelocity = (coyoteRigidbody.velocity.x * 0.95f);
+            if (Math.Round(alteredXVelocity, 10) != 0) x = alteredXVelocity; 
         }
 
         coyoteRigidbody.velocity = new Vector2(x, coyoteRigidbody.velocity.y);
