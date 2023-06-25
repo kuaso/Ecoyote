661b49d05e6fd176ea13ea4d28005ee0323e1506
Darshmallow <98087037+Darshmallow@users.noreply.github.com>
2023-06-25 05:12:29 -0700

    Camera action & temporary underground tiles

---
+++ Assets/CamerController.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamerController : MonoBehaviour
+{
+    [SerializeField] Transform coyote;
+    [SerializeField] Vector3 minValues, maxValues;
+
+    // Update is called once per frame
+    void Update()
+    {
+        float targetX = coyote.position.x + 1.5f;
+        if (targetX < minValues.x) {
+            targetX = minValues.x;
+        }
+        else if (targetX > maxValues.x) {
+            targetX = maxValues.x;
+        }
+
+        float targetY = coyote.position.y + 1.5f;
+        if (targetY < minValues.y)
+        {
+            targetY = minValues.y;
+        }
+        else if (targetY > maxValues.y)
+        {
+            targetY = maxValues.y;
+        }
+        transform.position = new Vector3(targetX, targetY, transform.position.z);
+
+    }
+}
