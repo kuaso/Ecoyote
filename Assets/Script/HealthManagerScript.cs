2c337955c20780eed7d15e05976c2fd4ab4f32fa
Cloudate9 <65006925+Cloudate9@users.noreply.github.com>
2023-06-25 00:08:07 -0700

    Fix bug of having negative health

--- Assets/Script/HealthManagerScript.cs
+++ Assets/Script/HealthManagerScript.cs
@@ -3,15 +3,17 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class LogicScript : MonoBehaviour
+public class HealthManagerScript : MonoBehaviour
 {
     private int _health = 3;
     public Image[] hearts;
     public Sprite fullHeart;
     public Sprite emptyHeart;
-    
+
     public int Health => _health;
 
+    // To use in other scripts, assign this script to public HealthManagerScript healthManager; in other scripts
+    
     public void Heal(int healing = 1)
     {
         if (healing < 0) throw new System.Exception("Healing cannot be negative");
@@ -19,11 +21,11 @@ public class LogicScript : MonoBehaviour
         else _health += healing;
         hearts[_health - 1].sprite = fullHeart;
     }
-    
+
     public void TakeDamage(int damage = 1)
     {
         if (damage < 0) throw new System.Exception("Damage cannot be negative");
-        _health -= damage;
+        if (_health > 0) _health -= damage;
         hearts[_health].sprite = emptyHeart;
         if (_health <= 0)
         {
@@ -35,5 +37,4 @@ public class LogicScript : MonoBehaviour
     {
         // TODO game over screen
     }
-
 }
