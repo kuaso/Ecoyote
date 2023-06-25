3922f8dfd10d91e0d90fdbc2064f8817b1d2f8eb
Cloudate9 <65006925+Cloudate9@users.noreply.github.com>
2023-06-24 18:56:06 -0700

    Fix errors

--- Assets/Background1Script.cs
+++ Assets/Background1Script.cs
@@ -2,11 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-<<<<<<<< Updated upstream:Assets/HeartsDisplayScript.cs
-public class HeartsDisplayScript : MonoBehaviour
-========
 public class Background1Script : MonoBehaviour
->>>>>>>> Stashed changes:Assets/Background1Script.cs
 {
     // Start is called before the first frame update
     void Start()
