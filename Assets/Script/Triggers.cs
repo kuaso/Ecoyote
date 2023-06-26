5973a318869a765aca3cc5eab14468e0287c42ca
Cloudate9 <65006925+Cloudate9@users.noreply.github.com>
2023-06-25 19:47:26 -0700

    Fix bug in trap speed

--- Assets/Script/Triggers.cs
+++ Assets/Script/Triggers.cs
@@ -1,14 +1,23 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ItemCollector : MonoBehaviour
+public class TriggersScript : MonoBehaviour
 {
     public HealthManagerScript health;
-    public PlayerMovement movement;
-    [SerializeField] Rigidbody2D coyote;
+    private PlayerMovement _movement;
+    private Rigidbody2D _coyoteRb;
+    
+    private float _origMaxMoveSpeed;
+    private bool _isTrapped;
+    void Start()
+    {
+        _coyoteRb = GetComponent<Rigidbody2D>();
+        _movement = GetComponent<PlayerMovement>();
+    }
 
-    private IEnumerator OnTriggerEnter2D(Collider2D collision)
+    private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("badCollectibles")) {
             Destroy(collision.gameObject);
@@ -20,22 +29,31 @@ public class ItemCollector : MonoBehaviour
             health.Heal();
         }
         else if (collision.gameObject.CompareTag("fire")) {
-            health.TakeDamage();
-            movement.maxMoveSpeed = 0.5f;
-            //todo: half second before it takes damage again
+            health.TakeDamage(); 
+            // todo: half second before it takes damage again
         }
         else if (collision.gameObject.CompareTag("trap")) {
             health.TakeDamage();
             
-            //slow down for 2 seconds
-            var origMaxMoveSpeed = movement.maxMoveSpeed;
-            var origJumpForce = movement.jumpForce;
-            movement.maxMoveSpeed *= 0.4f;
-            movement.jumpForce *= 0.4f;
+            // Only update original max speed and jump if the values have not been changed by the trap
+            if (!_isTrapped)
+            {
+                _isTrapped = true;
+                _origMaxMoveSpeed = _movement.maxMoveSpeed;
+            }
+            // Slow down for the entirety of the time that the player is in the trap
+            _movement.maxMoveSpeed = _origMaxMoveSpeed * 0.2f;
+        }
+    }
 
+    private IEnumerator OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("trap"))
+        {
+            // Make use wait for 2 seconds after exiting the trap before restoring normal speed
             yield return new WaitForSeconds(2);
-            movement.maxMoveSpeed = origMaxMoveSpeed;
-            movement.jumpForce = origJumpForce;
+            _movement.maxMoveSpeed = _origMaxMoveSpeed;
+            _isTrapped = false;
         }
     }
 }
