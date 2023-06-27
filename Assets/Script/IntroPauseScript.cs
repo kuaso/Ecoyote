5c2d0a7b022b2fb7e62ca7a8c6c47ce8f19c4e0e
Cloudate9 <65006925+Cloudate9@users.noreply.github.com>
2023-06-27 16:41:48 -0700

    Create way to escape out of intro scene

---
+++ Assets/Script/IntroPauseScript.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class IntroPauseScript : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+        }
+    }
+}
