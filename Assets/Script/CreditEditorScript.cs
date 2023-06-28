998e9f392c786e50545bc118bf018d7409be7df3
Cloudate9 <65006925+Cloudate9@users.noreply.github.com>
2023-06-27 20:19:14 -0700

    Fix credits being not in ratio

--- Assets/Script/CreditEditorScript.cs
+++ Assets/Script/CreditEditorScript.cs
@@ -24,31 +24,37 @@ public class CreditEditorScript : MonoBehaviour
         switch (coyoteRb.position.x)
         {
            
-            case var _ when coyoteRb.position.x >= maxX - 1f:
+            case var _ when coyoteRb.position.x >= maxX / 8 * 7.8:
                 _creditText.text = "Thank you for playing!";
                 break;
-            case var _ when coyoteRb.position.x >= maxX / 7: //fix this kenneth
-                _creditText.text = "Poison Apple image from PngAAA\nTrash images from Saphatthachat (Freepik)\nFire image from A455dc (Pixel Art Maker)\nTree Trunk image from Mroker (Deviant Art)\nBandits - Pixel Art from Sven Thole (Asset Store)\nShooting Enemy image from MintFresh (OpenGameArt)\nMeat image from cadmium_red (Freepik)\n";
+            case var _ when coyoteRb.position.x >= maxX / 8 * 7.1:
+                _creditText.text = "Bandits - Pixel Art from Sven Thole (Asset Store)\nShooting Enemy image from MintFresh (OpenGameArt)\nMeat image from cadmium_red (Freepik)\n";
+                _creditText.enableAutoSizing = true;
                 break;
-            case var _ when coyoteRb.position.x >= maxX / 7:
+            case var _ when coyoteRb.position.x >= maxX / 8 * 6.3:
+                _creditText.text =
+                    "Poison Apple image from PngAAA\nTrash images from Saphatthachat (Freepik)\nFire image from A455dc (Pixel Art Maker)\nTree Trunk image from Mroker (Deviant Art)";
+                _creditText.enableAutoSizing = true;
+                break;
+            case var _ when coyoteRb.position.x >= maxX / 8 * 6:
                 _creditText.text = "Assets credits";
                 break;
-            case var _ when coyoteRb.position.x >= maxX / 6 * 5.3:
+            case var _ when coyoteRb.position.x >= maxX / 8 * 5.3:
                 _creditText.text = "David Eirew (Narration)";
                 break;
-            case var _ when coyoteRb.position.x >= maxX / 6 * 5:
+            case var _ when coyoteRb.position.x >= maxX / 8 * 5:
                 _creditText.text = "Special Thanks";
                 break;
-            case var _ when coyoteRb.position.x >= maxX / 6 * 4:
+            case var _ when coyoteRb.position.x >= maxX / 8 * 4:
                 _creditText.text = "Soren Caron (Code)";
                 break;
-            case var _ when coyoteRb.position.x >= maxX / 6 * 3:
+            case var _ when coyoteRb.position.x >= maxX / 8 * 3:
                 _creditText.text = "Kenneth Goh (Code)";
                 break;
-            case var _ when coyoteRb.position.x >= maxX / 6 * 2:
+            case var _ when coyoteRb.position.x >= maxX / 8 * 2:
                 _creditText.text = "Daphne Zhang (Art)";
                 break;
-            case var _ when coyoteRb.position.x >= maxX / 6:
+            case var _ when coyoteRb.position.x >= maxX / 8:
                 _creditText.text = "Alice Wang (Code)";
                 break;
             case var _ when coyoteRb.position.x >= maxX / 30:
